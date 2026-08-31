a5f35cd710222c0864db6e82bdfe8cdad2cfa8e9
manx o matt <manxomatt@gmail.com>
2026-08-31 10:54:52 +0700

    feat: import Request class in MaintenanceScheduleController

--- modules/Maintenance/Http/Controllers/MaintenanceScheduleController.php
+++ modules/Maintenance/Http/Controllers/MaintenanceScheduleController.php
@@ -4,6 +4,7 @@ namespace Modules\Maintenance\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use Inertia\Response;
